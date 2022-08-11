b3fcdfbc7fa652fe691fa35df09ad76f488e358e
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-08-11 15:55:23 +0100

    Fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_083959) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_080056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_083959) do
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -35,8 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_083959) do
     t.integer "author_id"
     t.string "title"
     t.string "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -45,10 +43,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_083959) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "users"
 end
