eb040fc3c9d675a47ca752061262cdbfd218dec1
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-08-11 18:22:23 +0100

    Delete previous migrations

--- db/migrate/20220811132731_add_author_ref_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorRefToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :posts, :author_id, null: false, foreign_key: true
-  end
-end
--- db/migrate/20220811132801_add_author_ref_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorRefToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :likes, :author_id, null: false, foreign_key: true
-  end
-end
