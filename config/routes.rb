c300b975e81eab2fb2052b8b01559c1cc799c6a3
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-08-18 14:48:06 +0100

    Nest posts inside of users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:show, :index] 
-  resources :posts, only: [:show, :index]
+  resources :users, only: [:show, :index] do
+     resources :posts, only: [:show, :index]
+  end
 end
