026c2f1b6565c19fab55fd940b09f3aa229bfe20
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-08-30 08:09:56 +0100

    Add Users Show spec

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe 'show page' do
   before :each do
-    @user_first = User.create(name: 'Temitope', photo: 'user.png', bio: 'This is first user')
+    @user_first = User.create(name: 'Temitope', photo: 'user.png', bio: 'This is first user',
+                              email: 'topeogunleye1@gmail.com', password: 'Ygpcznj3BCSt$&bc')
 
     @post_first = @user_first.posts.new(title: 'Self Improvement is the goal', text: 'This is my first post')
     @post_first.likes_counter = 0
@@ -18,6 +19,11 @@ describe 'show page' do
     @post_third.likes_counter = 0
     @post_third.comments_counter = 0
     @post_third.save
+
+    visit root_path
+    fill_in 'Email', with: 'topeogunleye1@gmail.com'
+    fill_in 'Password', with: 'Ygpcznj3BCSt$&bc'
+    click_button 'Log in'
   end
 
   it 'check users#show path' do
