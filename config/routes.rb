233d7db9d4fad08a58ba5e70d67f5077b6d34261
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-10 14:46:59 +0100

    Revert "Milestone 9: Add Devise"

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
-  resources :users, only: [:index, :show] do
+  resources :users do
      resources :posts, only: [:show, :index, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
-    end
   end
+end
 end
